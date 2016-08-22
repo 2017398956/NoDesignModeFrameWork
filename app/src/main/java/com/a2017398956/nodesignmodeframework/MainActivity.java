6f2d5eadb90d4e4050109ca92b2f203d63b85c67
choochoo <2017398956@qq.com>
2016-08-22 23:41:43 +0800

    更新lol

--- app/src/main/java/com/a2017398956/nodesignmodeframework/MainActivity.java
+++ app/src/main/java/com/a2017398956/nodesignmodeframework/MainActivity.java
@@ -1,15 +1,19 @@
 package com.a2017398956.nodesignmodeframework;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.a2017398956.nodesignmodeframework.exception.test.ResultInfoExceptionActivity;
 import com.nfl.libraryoflibrary.utils.CustomBroadcastSender;
+import com.nfl.libraryoflibrary.utils.PhoneInfoTool;
 import com.nfl.libraryoflibrary.utils.RootDetector;
 import com.nfl.libraryoflibrary.utils.ToastTool;
 import com.nfl.libraryoflibrary.view.BaseActivity;
+import com.nfl.libraryoflibrary.view.CustomHorizontalLeftSlidingView;
 
 public class MainActivity extends BaseActivity {
 
@@ -26,6 +30,18 @@ public class MainActivity extends BaseActivity {
     private void initView() {
         tv_test_info = (TextView) findViewById(R.id.tv_test_info);
         tv_test_info.setText("手机是否root：" + new RootDetector().isDeviceRooted());
+        CustomHorizontalLeftSlidingView customHorizontalLeftSlidingView ;
+        TextView tv_01 = new TextView(this) ;
+        tv_01.setWidth(PhoneInfoTool.getScreenHeight(this));
+        tv_01.setText("tv_01");
+        tv_01.setTextColor(Color.WHITE);
+        tv_01.setBackgroundColor(Color.YELLOW);
+        TextView tv_02 = new TextView(this) ;
+        tv_02.setText("tv_02");
+        tv_02.setTextColor(Color.WHITE);
+        tv_02.setBackgroundColor(Color.RED);
+//        customHorizontalLeftSlidingView = new CustomHorizontalLeftSlidingView(this , tv_01 , tv_02 ) ;
+//        ((RelativeLayout) tv_test_info.getParent()).addView(customHorizontalLeftSlidingView);
     }
 
     public void next(View view) {
