b3e6c5ebe3ba1c3d60db8ac1644a397907f6489a
choochoo <2017398956@qq.com>
2016-09-06 18:01:22 +0800

    添加multidex以解决65536问题

--- app/src/main/java/com/a2017398956/nodesignmodeframework/MyApplication.java
+++ app/src/main/java/com/a2017398956/nodesignmodeframework/MyApplication.java
@@ -1,6 +1,8 @@
 package com.a2017398956.nodesignmodeframework;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.nfl.libraryoflibrary.constant.ApplicationContext;
 import com.nfl.libraryoflibrary.utils.CustomActivityLifecycleCallbacks;
@@ -17,4 +19,11 @@ public class MyApplication extends Application {
         ApplicationContext.applicationContext = this ;
         registerActivityLifecycleCallbacks(new CustomActivityLifecycleCallbacks());
     }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this) ;
+    }
+
 }
