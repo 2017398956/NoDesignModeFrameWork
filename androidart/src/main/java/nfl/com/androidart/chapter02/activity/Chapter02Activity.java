26abfcc0c9f7eb6e3d71d29f880911a6bfe8a9ad
choochoo <2017398956@qq.com>
2017-05-08 18:05:04 +0800

    update

---
+++ androidart/src/main/java/nfl/com/androidart/chapter02/activity/Chapter02Activity.java
@@ -0,0 +1,16 @@
+package nfl.com.androidart.chapter02.activity;
+
+import android.os.Bundle;
+
+import com.nfl.libraryoflibrary.view.activity.CommonActionBarActivity;
+
+import nfl.com.androidart.R;
+
+public class Chapter02Activity extends CommonActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter02);
+    }
+}
